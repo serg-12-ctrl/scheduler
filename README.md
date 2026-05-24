b50bfcc328897f968e2a9682ce8a46672f5fdea7
serg-12-ctrl <srhiganov@gmail.com>
2026-05-24 16:22:43 +0800

    finish

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # scheduler
+
+https://pypi.org/project/schedule/
+
+https://schedule.readthedocs.io/en/stable/index.html
--- my_scheduler.py
+++ my_scheduler.py
@@ -2,5 +2,19 @@ import schedule
 import time
 from datetime import datetime
 
-def job():
-    print(f"Задача выполнена! Время: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+def test_job():
+    
+    current_time = datetime.now().strftime('%H:%M:%S')
+    print(f" Тест успешный! Функция сработала в: {current_time}")
+
+    if datetime.now().day == 24:
+        print(f"Задача выполнена! Время: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+#schedule.every(5).seconds.do(test_job)
+schedule.every().day.at("16:07").do(test_job)
+
+
+print("Планировщик запущен. Ждем...")
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
